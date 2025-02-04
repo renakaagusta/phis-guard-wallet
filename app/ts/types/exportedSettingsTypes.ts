e2f4b6147c26fdea2984e07c973f380ab214ba25
renakaagusta <renakaagusta28@gmail.com>
2025-02-04 14:28:11 +0700

    feat: remove collapsible, log handlers, and ethereum name service

--- app/ts/types/exportedSettingsTypes.ts
+++ app/ts/types/exportedSettingsTypes.ts
@@ -1,9 +1,6 @@
 import * as funtypes from 'funtypes'
-import { AddressBookEntries, ContactEntries } from './addressBookTypes.js'
-import { RpcNetwork } from './rpc.js'
 import { ModifyAddressWindowState } from './visualizer-types.js'
-import { WebsiteAccessArray } from './websiteAccessTypes.js'
-import { EthereumAddress, EthereumQuantity, LiteralConverterParserFactory, OptionalEthereumAddress } from './wire-types.js'
+import { EthereumAddress, LiteralConverterParserFactory } from './wire-types.js'
 
 export type Page = funtypes.Static<typeof Page>
 export const Page = funtypes.Union(
@@ -21,89 +18,3 @@ export const ActiveAddress = funtypes.ReadonlyObject({
 	address: EthereumAddress,
 	askForAddressAccess: funtypes.Union(funtypes.Boolean, funtypes.Literal(undefined).withParser(LiteralConverterParserFactory(undefined, true))),
 }).asReadonly()
-
-type ActiveAddressArray = funtypes.Static<typeof ActiveAddressArray>
-const ActiveAddressArray = funtypes.ReadonlyArray(ActiveAddress)
-
-export type ExportedSettings = funtypes.Static<typeof ExportedSettings>
-export const ExportedSettings = funtypes.Union(
-	funtypes.ReadonlyObject({
-		name: funtypes.Literal('InterceptorSettingsAndAddressBook'),
-		version: funtypes.Literal('1.0'),
-		exportedDate: funtypes.String,
-		settings: funtypes.ReadonlyObject({
-			activeSimulationAddress: OptionalEthereumAddress,
-			activeChain: EthereumQuantity,
-			useSignersAddressAsActiveAddress: funtypes.Boolean,
-			websiteAccess: WebsiteAccessArray,
-			simulationMode: funtypes.Boolean,
-			addressInfos: ActiveAddressArray,
-			contacts: funtypes.Union(funtypes.Undefined, ContactEntries),
-			useTabsInsteadOfPopup: funtypes.Boolean,
-		})
-	}),
-	funtypes.ReadonlyObject({
-		name: funtypes.Literal('InterceptorSettingsAndAddressBook'),
-		version: funtypes.Literal('1.1'),
-		exportedDate: funtypes.String,
-		settings: funtypes.ReadonlyObject({
-			activeSimulationAddress: OptionalEthereumAddress,
-			rpcNetwork: RpcNetwork,
-			useSignersAddressAsActiveAddress: funtypes.Boolean,
-			websiteAccess: WebsiteAccessArray,
-			simulationMode: funtypes.Boolean,
-			addressInfos: ActiveAddressArray,
-			contacts: funtypes.Union(funtypes.Undefined, ContactEntries),
-			useTabsInsteadOfPopup: funtypes.Boolean,
-		})
-	}),
-	funtypes.ReadonlyObject({
-		name: funtypes.Literal('InterceptorSettingsAndAddressBook'),
-		version: funtypes.Literal('1.2'),
-		exportedDate: funtypes.String,
-		settings: funtypes.ReadonlyObject({
-			activeSimulationAddress: OptionalEthereumAddress,
-			rpcNetwork: RpcNetwork,
-			useSignersAddressAsActiveAddress: funtypes.Boolean,
-			websiteAccess: WebsiteAccessArray,
-			simulationMode: funtypes.Boolean,
-			addressInfos: ActiveAddressArray,
-			contacts: funtypes.Union(funtypes.Undefined, ContactEntries),
-			useTabsInsteadOfPopup: funtypes.Boolean,
-			metamaskCompatibilityMode: funtypes.Boolean,
-		})
-	}),
-	funtypes.ReadonlyObject({
-		name: funtypes.Literal('InterceptorSettingsAndAddressBook'),
-		version: funtypes.Literal('1.3'),
-		exportedDate: funtypes.String,
-		settings: funtypes.ReadonlyObject({
-			activeSimulationAddress: OptionalEthereumAddress,
-			rpcNetwork: RpcNetwork,
-			openedPage: Page,
-			useSignersAddressAsActiveAddress: funtypes.Boolean,
-			websiteAccess: WebsiteAccessArray,
-			simulationMode: funtypes.Boolean,
-			addressInfos: ActiveAddressArray,
-			contacts: funtypes.Union(funtypes.Undefined, ContactEntries),
-			useTabsInsteadOfPopup: funtypes.Boolean,
-			metamaskCompatibilityMode: funtypes.Boolean,
-		})
-	}),
-	funtypes.ReadonlyObject({
-		name: funtypes.Literal('InterceptorSettingsAndAddressBook'),
-		version: funtypes.Literal('1.4'),
-		exportedDate: funtypes.String,
-		settings: funtypes.ReadonlyObject({
-			activeSimulationAddress: OptionalEthereumAddress,
-			rpcNetwork: RpcNetwork,
-			openedPage: Page,
-			useSignersAddressAsActiveAddress: funtypes.Boolean,
-			websiteAccess: WebsiteAccessArray,
-			simulationMode: funtypes.Boolean,
-			addressBookEntries: AddressBookEntries,
-			useTabsInsteadOfPopup: funtypes.Boolean,
-			metamaskCompatibilityMode: funtypes.Boolean,
-		})
-	}),
-)
